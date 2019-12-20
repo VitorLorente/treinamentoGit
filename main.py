d9bb03bb787e66be81d73ac10cdda47511084672
vitorlorente <lorente.sociologia@gmail.com>
2019-12-20 10:12:09 -0300

    Initial commit.

---
+++ main.py
@@ -0,0 +1,52 @@
+from decimal import Decimal
+
+class Funcionario:
+    def __init__(self, nome, idade, cargo, salario):
+        self.nome = nome
+        self.idade = idade
+        self.cargo = cargo
+        self.salario = salario
+
+    def setNome(self, nome):
+        self.nome = nome
+
+    def setIdade(self, idade):
+        self.idade = idade
+
+    def setCargo(self, cargo):
+        self.cargo = cargo
+
+    def setSalario(self, salario):
+        self.salario = salario
+
+    def getNome(self):
+        return self.nome
+
+    def getIdade(self):
+        return self.idade
+
+    def getCargo(self):
+        return self.cargo
+
+    def getSalario(self):
+        return format(self.salario, '.2f')
+
+    def apresentacao(self):
+        return f'Olá! Eu sou o {self.getNome()} e tenho {self.getIdade()} anos. Trabalho como {self.getCargo()} e ganho R$ {self.getSalario()}.'
+
+    def decimoTerceiro(self):
+        return (self.salario * 2)
+
+    def bonus(self, avaliacao):
+        if 1 <= avaliacao < 2:
+            return format(self.salario, '.2f')
+        elif 2 <= avaliacao < 3:
+            return format(self.salario * 2, '.2f')
+        elif 3 <= avaliacao < 4:
+            return format(self.salario * 3, '.2f')
+        elif 4 <= avaliacao < 5:
+            return format(self.salario * 4, '.2f')
+        elif avaliacao == 5:
+            return format(self.salario * 5, '.2f')
+        else:
+            return 'Pontuação inválida.'
